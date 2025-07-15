6521b76ff42f5340531a3f89de5c9d1e1ed3a122
ThePasha <pashaofficial37@gmail.com>
2025-07-15 12:06:04 +0300

    Delete İllegalizm Music Bot directory

--- İllegalizm Music Bot/events/Player/playerFinish.js
+++
@@ -1,8 +0,0 @@
-module.exports = (queue) => { //discord.gg/vsc ❤️ oxyinc, can066
-    if (queue.metadata.lyricsThread) {
-        queue.metadata.lyricsThread.delete();
-        queue.setMetadata({
-            channel: queue.metadata.channel
-        });
-    }
-}
